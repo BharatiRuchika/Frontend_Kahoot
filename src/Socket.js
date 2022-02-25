ea07a94bc8d4c045003bc19963d0ce185b8f2b48
BharatiRuchika <ruchikabharaticdac@gmail.com>
2022-02-26 02:01:24 +0530

    fhj

--- src/Socket.js
+++ src/Socket.js
@@ -12,7 +12,7 @@ var connectionOptions = {
 
 // 
 // let socket = io.connect('https://my-kahoot-backend.herokuapp.com/',connectionOptions);
-let socket = io("https://my-kahoot-backend.herokuapp.com");
+let socket = io("https://secure-ravine-99917.herokuapp.com/");
 // const socket = io.connect("https://my-kahoot-backend.herokuapp.com/", { secure: true, reconnection: true, rejectUnauthorized: false });
 console.log("socket",socket);
 export default socket;
